ca1b406584aa06f7b93c53e9a19948a1da701f63
birkan31 <49659581+birkan31@users.noreply.github.com>
2019-04-16 00:10:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,7 @@ const config = require("./config.json");
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
-
-    console.log (`${bot.user.username} is online!)
+    console.log (`${bot.user.username} is online!`)
     bot.user.setActivity("Prefix is '-'!", {type: "HIZLANIYOR"});
 });
 
