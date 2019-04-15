ce486fccc62b294a75413c43a3043c7372f60eee
birkan31 <49659581+birkan31@users.noreply.github.com>
2019-04-15 23:54:28 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+
+    console.log (`${bot.user.username} is online!)
+    bot.user.setActivity("Prefix is '-'!", {type: "HIZLANIYOR"});
+});
+
+bot.login(config.token);
